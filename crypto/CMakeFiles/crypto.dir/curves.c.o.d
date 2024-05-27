cbfde40a1af21c406dd4162fb6238874b0174f42
rxbryan <rxbryn@gmail.com>
2024-05-27 11:26:30 +0100

    chore: clean repo

--- crypto/CMakeFiles/crypto.dir/curves.c.o.d
+++
@@ -1,5 +0,0 @@
-crypto/CMakeFiles/crypto.dir/curves.c.o: \
- /home/rxbryan/work/sob/taproot_segwit_poc/crypto/curves.c \
- /usr/include/stdc-predef.h \
- /home/rxbryan/work/sob/taproot_segwit_poc/crypto/curves.h \
- /home/rxbryan/work/sob/taproot_segwit_poc/crypto/options.h
